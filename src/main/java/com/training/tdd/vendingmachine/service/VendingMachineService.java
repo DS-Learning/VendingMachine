0ec92b64cf1f79abcc7279e305c34fe580e632da
Napasin Yamwat <triplesic@gmail.com>
2017-11-12 23:03:58 +0700

    delete unused code

--- src/main/java/com/training/tdd/vendingmachine/service/VendingMachineService.java
+++ src/main/java/com/training/tdd/vendingmachine/service/VendingMachineService.java
@@ -14,31 +14,6 @@ public class VendingMachineService {
 
     ProductRepository legacyProductRepo = new ProductRepository();
 
-    public Map<String, Object> buyProduct(String string, long l) {
-
-        boolean hasProduct = hasProduct(string);
-
-        Map<String, Object> expected = new HashMap<>();
-
-        if (hasProduct) {
-            expected.put("product", string);
-            expected.put("exchange", null);
-            expected.put("message", "Thank you!");
-        } else {
-            expected.put("product", null);
-            expected.put("exchange", Arrays.asList(15L));
-            expected.put("message", "Sorry! no product");
-        }
-
-        return expected;
-    }
-
-    private boolean hasProduct(String product) {
-
-        return legacyProductRepo.hasProduct(product);
-
-    }
-
     public String getAllProducts() {
 
         List<Product> productList = legacyProductRepo.getAllProducts();
